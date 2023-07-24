c928d031c134723b336b77b4148c62ae5f1a1bc6
Nonato Sousa <nonato.sousa@antikytera.com>
2023-07-24 15:26:06 -0300

    Initial Commit

---
+++ SaveMediaToGallery/SaveMediaToGallery.h
@@ -0,0 +1,18 @@
+//
+//  SaveMediaToGallery.h
+//  SaveMediaToGallery
+//
+//  Created by Nonato Sousa on 24/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SaveMediaToGallery.
+FOUNDATION_EXPORT double SaveMediaToGalleryVersionNumber;
+
+//! Project version string for SaveMediaToGallery.
+FOUNDATION_EXPORT const unsigned char SaveMediaToGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaveMediaToGallery/PublicHeader.h>
+
+
